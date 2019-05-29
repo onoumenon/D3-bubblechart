61cc68e87cc2a4a24227784dc7655bb3719aea08
onoumenon <tanhuitian@gmail.com>
2019-05-29 20:28:49 +0800

    Create the D3 toolbar helper function

---
+++ src/components/D3_components/ToolbarHelper.js
@@ -0,0 +1,15 @@
+import * as d3 from "d3";
+
+export const setupToolbar = chart => {
+  d3.select("#toolbar")
+    .selectAll(".button")
+    .on("click", function() {
+      d3.selectAll(".button").classed("active", false);
+
+      let button = d3.select(this);
+      button.classed("active", true);
+
+      let buttonId = button.attr("id");
+      return buttonId;
+    });
+};
